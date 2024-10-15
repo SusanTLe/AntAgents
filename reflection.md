66e385e5edf95538a70e7b16370e0cc49a62902c
Susan Le <susanle@susans-air.middlebury.edu>
2024-10-15 18:39:52 -0400

    For gradescope

--- reflection.md
+++ reflection.md
@@ -1,13 +1,16 @@
 # Reflection
 
 - What (if anything) did you struggle with?
+I had put my if statements to check the bounds inside of the beginShape() and endShape(), which made it react really weirdly and jump around all over the place. I put it on the outside to fix it.
 
 - What (if anything) still is not complete?
+I think it is mostly complete. I added the third optional parameter to noise to add the 3d level.
 
 - What questions (if any) do you have about this assignment?
+None
 
 - What do you feel like you understand better after having completed this?
-
+I understand better how to use noise to add a complexity to a piece that looks organic.
 
 
 
--- sketch.js
+++ sketch.js
@@ -1,7 +1,73 @@
+let noiseScale = 0.03;
+let antArray;
+let antCount = 100;
+
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(400, 400);
+  background(0);
+  antArray = [];
+  for (let i = 0; i < antCount; i++){
+      antArray.push(new Ant());
+  }
 }
 
 function draw() {
-  background(255);
+  stroke(255,15);
+  for (let ant of antArray) {
+  ant.update();
+  }
 }
+
+class Ant {
+  
+  constructor() {
+    this.position = createVector(random(width), random(height));
+    this.life = random(50,100);
+    this.layer = 0;
+  }
+    
+  
+  //method
+  update(){
+    
+    if (this.life > 0) {
+    //scaling with 0.03
+       let noiseValue = noise(this.position.x * noiseScale, this.position.y * noiseScale,this.layer);
+    
+    //using map function to scale from 0,1 to 0,2PI
+       let angle = map(noiseValue, 0, 1, 0, TWO_PI);
+    
+    //creating unit vector from angle
+      let direction = p5.Vector.fromAngle(angle, 2);
+    
+      beginShape(LINES);
+      vertex(this.position.x,this.position.y);
+      this.position.add(direction);
+      vertex(this.position.x,this.position.y);
+      endShape();
+      if(this.position.x > width){
+        this.position.x = 0;
+      }
+      else if(this.position.x < 0){
+        this.position.x = width;
+      }
+    
+      if(this.position.y > height){
+        this.position.y = 0;
+      }
+      else if(this.position.y < 0){
+        this.position.y = height;
+      }
+      this.life -= 1;
+    }
+    
+    else if(this.life <= 0){
+      this.position = createVector(random(width), random(height));
+      this.life = random(50,100);
+      this.layer += 1;
+    }
+  
+
+  }
+}
+
